44e27db84671ac38d22acb82054019589a72a1ae
Sursh <6990413@mail.ru>
2019-03-26 20:34:51 +0300

    HW06 First Review with corrected database credentials

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -8,7 +8,9 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
@@ -16,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JdbcMealRepositoryImpl implements MealRepository {
 
     private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
@@ -25,6 +28,9 @@ public class JdbcMealRepositoryImpl implements MealRepository {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     private final SimpleJdbcInsert insertMeal;
+
+    @Autowired
+    private DataSourceTransactionManager transactionManager;
 
     @Autowired
     public JdbcMealRepositoryImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -37,6 +43,7 @@ public class JdbcMealRepositoryImpl implements MealRepository {
     }
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
         MapSqlParameterSource map = new MapSqlParameterSource()
                 .addValue("id", meal.getId())
@@ -61,6 +68,7 @@ public class JdbcMealRepositoryImpl implements MealRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
         return jdbcTemplate.update("DELETE FROM meals WHERE id=? AND user_id=?", id, userId) != 0;
     }
---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,84 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+
+@Controller
+@RequestMapping(value = "/meals")
+public class JspMealController extends AbstractMealController {
+
+    @GetMapping
+    public String getAllMeals(HttpServletRequest request) {
+        request.setAttribute("meals", getAll());
+        return "meals";
+    }
+
+    @GetMapping(params = "action=delete")
+    public String deleteMeal(@RequestParam("id") String mealId) {
+        delete(Integer.parseInt(mealId));
+        return "redirect:meals";
+    }
+
+    @GetMapping(params = "action=create")
+    public String createMeal(Model model) {
+        final Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        model.addAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @GetMapping(params = "action=update")
+    public String updateMeal(Model model, @RequestParam("id") String mealId) {
+        final Meal meal = get(Integer.parseInt(mealId));
+        model.addAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @PostMapping
+    public String addingNewMeal(@RequestParam("dateTime") String dateTime,
+                                @RequestParam(value = "description") String description,
+                                @RequestParam("id") String id,
+                                @RequestParam("calories") String calories
+    ) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(dateTime),
+                description,
+                Integer.parseInt(calories));
+        System.out.println(description);
+        if (StringUtils.isEmpty(id)) {
+            create(meal);
+        } else {
+            update(meal, Integer.parseInt(id));
+        }
+        return "redirect:meals";
+    }
+
+    @PostMapping(value = "", params = "action=filter")
+    public String getFiltered(@RequestParam("startDate") String startDate,
+                              @RequestParam("endDate") String endDate,
+                              @RequestParam("startTime") String startTime,
+                              @RequestParam("endTime") String endTime, Model model) {
+
+        LocalDate startLocalDate = parseLocalDate(startDate);
+        LocalDate endLocalDate = parseLocalDate(endDate);
+        LocalTime startLocalTime = parseLocalTime(startTime);
+        LocalTime endLocalTime = parseLocalTime(endTime);
+        model.addAttribute("meals", getBetween(startLocalDate, startLocalTime, endLocalDate, endLocalTime));
+        return "meals";
+    }
+
+}
